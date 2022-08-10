be7e7f5dc03a035bbe38378baab5951e1552f19d
Pawan Yadav <pawanyadav8053@gmail.com>
2022-08-10 15:49:59 +0530

    Update: Multithreading using concurrent.futures
    - Also rename logging.py to my_logging.py

--- multithreading.py
+++ multithreading.py
@@ -1,22 +1,20 @@
 """Reference: Corey Schafer's video on YouTube"""
-import threading
+import concurrent.futures
 import time
 
 def do_something(seconds):
     print(f"Sleeping {seconds} second(s)...")
     time.sleep(seconds)
-    print("Done sleeping")
+    return f"Done sleeping...{seconds} second(s)"
 
 start = time.perf_counter()
 
-threads = []
-for _ in range(10):
-    t = threading.Thread(target=do_something, args=[1.5])
-    t.start()
-    threads.append(t)
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    seconds = [5, 4, 3, 2, 1]
+    results = [executor.submit(do_something, sec) for sec in seconds]
 
-for thread in threads:
-    thread.join()
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
 
 finish = time.perf_counter()
 print(f"Finished in {round(finish-start, 2)} second(s)")
