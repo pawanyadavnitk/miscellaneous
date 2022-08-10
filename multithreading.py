3eb1a99288097210980ecddd3ed9715c7cf16e12
Pawan Yadav <pawanyadav8053@gmail.com>
2022-08-10 16:01:10 +0530

    Update: Use map method instead of submit method

--- multithreading.py
+++ multithreading.py
@@ -11,10 +11,12 @@ start = time.perf_counter()
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     seconds = [5, 4, 3, 2, 1]
-    results = [executor.submit(do_something, sec) for sec in seconds]
+    # the map method returns the results directly,
+    # whereas the submit method returns the future objects
+    results = executor.map(do_something, seconds)
 
-    for f in concurrent.futures.as_completed(results):
-        print(f.result())
+    for result in results:
+        print(result)
 
 finish = time.perf_counter()
 print(f"Finished in {round(finish-start, 2)} second(s)")
